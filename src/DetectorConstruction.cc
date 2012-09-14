01151c224891eb1f3a55e17245ec928f6d649559
Riccardo Iaconelli <riccardo@kde.org>
2012-09-14 20:10:07 +0200

    some tries
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -107,7 +107,7 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 
 
     /*-------Sensor-------*/
-
+    airgap = 100; // TODO HACK FIXME
     G4Box *TA_box = new G4Box("Sensor", 0.5 * crystal_x, 0.5 * crystal_y, 0.5 * airgap);
     G4LogicalVolume *TA_log  = new G4LogicalVolume(TA_box, Air, "Sensor", 0, 0, 0);
     G4VPhysicalVolume *TA_phys = new G4PVPlacement(0, G4ThreeVector(0, 0, 0.5 * (crystal_height + airgap)), TA_log, "Sensor", expHall_log, false, 0);
